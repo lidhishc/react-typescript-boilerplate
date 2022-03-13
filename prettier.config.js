723f62e560dc06f267c0de79041608253421eba0
lidhishc <lidhishc@gmail.com>
2022-03-13 13:50:31 +0530

    fix: prettier update

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    printWidth: 80,
-    tabWidth: 2,
-    useTabs: false,
-    semi: true,
-    singleQuote: true,
-    trailingComma: 'all',
-    arrowParens: 'avoid',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
 };
